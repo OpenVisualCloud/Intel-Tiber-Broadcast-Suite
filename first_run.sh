d677ea20bba65cd52473c6af01464a6c992c2b15
DawidWesierski4 <92673141+DawidWesierski4@users.noreply.github.com>
2024-05-13 13:04:43 +0200

    fix query for nicctl.sh to work with root user (#42)
    Fix the query of first_run.sh to run with the root user via the
    $HOME environment variable instead of the hardcoded absolute path
    of /home

--- first_run.sh
+++ first_run.sh
@@ -9,5 +9,5 @@ for f in /sys/devices/system/node/node*; do echo 1 > "$f/hugepages/hugepages-104
 docker network create --subnet 192.168.2.0/24 --gateway 192.168.2.100 -o parent=ens801f0 my_net_801f0
 output=$(lspci | grep "Ethernet controller: Intel Corporation Ethernet Controller" | awk '{print "0000:"$1}')
 IFS=$'\n'
-while IFS= read -r line; do find /home/ -name "nicctl.sh" -exec {} create_vf $line \;; done <<< $output
+while IFS= read -r line; do find ${HOME} -name "nicctl.sh" -exec {} create_vf $line \;; done <<< $output
 chmod 777 -R /dev/vfio
