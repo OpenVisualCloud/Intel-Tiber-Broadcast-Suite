1806d6275342ae49e1197aea6f600cbf60350b3c
Lukas G <20104521+zLukas@users.noreply.github.com>
2024-05-29 15:31:25 +0200

    syntax error fix (#60)
    * syntax error fix
    
    * Update trivy.sh

--- jenkins/scripts/trivy.sh
+++ jenkins/scripts/trivy.sh
@@ -8,8 +8,15 @@ mkdir Trivy
 
 # get the latest video_production_image 
 IMAGE_TAR=${1}
+# vulnerability scans
 trivy image --no-progress \
             --exit-code 0 \
             --format table \
-            -o Trivy/Trivy_Dockerfile.spdx \
+            -o Trivy/Trivy_vulnerability.Dockerfile.log \
+            --input ${IMAGE_TAR%%.tar}
+# spdx licence scans 
+trivy image --no-progress \
+            --exit-code 0 \
+            --format spdx-json \
+            -o Trivy/Trivy_spdx.Dockerfile.json \
             --input ${IMAGE_TAR%%.tar}
