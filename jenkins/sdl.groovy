404b08cc75732808055cdba88793f4a01ec08efc
zLukas <lukasz.grab@intel.com>
2024-06-06 10:17:29 +0200

    protex scan added

--- jenkins/sdl.groovy
+++ jenkins/sdl.groovy
@@ -1,4 +1,4 @@
-@Library(['jenkins-common-pipelines', 'pact-shared-library']) _
+@Library(['jenkins-common-pipelines', 'pact-shared-library', 'abi']) _
 
 def setSdlStatusCheck(String state){
     withCredentials([usernamePassword(
@@ -15,14 +15,16 @@ def setSdlStatusCheck(String state){
     }
 }
 
+def DOCKER_ABI="docker run --rm -v \$(pwd):/opt/ ${env.ABI_IMAGE} /bin/bash -c \"cd /opt/;"
+
 def scan(String type, String tar_image){
 
-    def abi_image="amr-registry.caas.intel.com/owr/abi_lnx:3.0.0"
     def _cmd =[
         hadolint: "jenkins/scripts/hadolint.sh" ,
-        trivy: "jenkins/scripts/trivy.sh ${tar_image}",
+        trivy: "jenkins/scripts/trivy.sh ${env.ABI_IMAGE}",
         schellcheck: "jenkins/scripts/shellcheck.sh",
-        mcAffee: "docker run --rm -v \$(pwd):/opt/ ${abi_image} /bin/bash -c \"cd /opt/; jenkins/scripts/mcafee_scan.sh\""
+        mcAffee: "${DOCKER_ABI} jenkins/scripts/mcafee_scan.sh\""
+        protex: "${DOCKER_ABI} jenkins/scripts/protex_scan.sh\""
     ]
     def _artifacts_path =[
         hadolint: "Hadolint/hadolint-Dockerfile*" ,
@@ -37,6 +39,7 @@ def scan(String type, String tar_image){
 }
 
 
+
 pipeline {
     agent {
         label 'video-production-build'
@@ -48,8 +51,14 @@ pipeline {
         string(name: 'relative_dir', defaultValue: '', description: 'jenkins workspace with codespace ')
     }
     environment {
+        ABI_IMAGE="amr-registry.caas.intel.com/owr/abi_lnx:3.0.0"
         GITHUB_PR_HEAD_SHA = "${params.github_pr_head_sha}"
         GITHUB_REPO_STATUS_URL = "${params.github_repo_status_url}"
+        PROTEX_SERVER = "gerprotex012.devtools.intel.com"
+        EVIDENCEFOLDER = "InternalEvidence"
+        PROTEX_PROJECT = "c_broadcastsuiteformediaentertainment_33366"
+        CRED_DEFAULT = "build_sie"
+        CRED_DEFAULT_EMAIL = "build_sie-email"
     }
     stages {
         stage("set status"){
@@ -98,6 +107,34 @@ pipeline {
                             }
                         }
                     } 
+                }
+                stage("Protex"){
+                    steps{
+                        script{
+                            de62b8cb-2c0a-4a67-83ea-cc51c7486c05
+                            withCredentials([usernamePassword(
+                                credentialsId: 'de62b8cb-2c0a-4a67-83ea-cc51c7486c05',
+                                usernameVariable: 'USERNAME',
+                                passwordVariable: 'PASSWORD')]){
+                                dir(params.relative_dir){
+                                    sh"""
+                                    ${DOCKER_ABI} abi ip_scan scan \
+                                            --scan_server ${env.PROTEX_SERVER} \
+                                            --scan_project ${env.PROTEX_PROJECT} \
+                                            --username "${USERNAME}" \
+                                            --password "${PASSWORD}" \
+                                            --root_dir ${WORKSPACE} \
+                                            --ing_path "." \
+                                            --report_type xlsx \
+                                            --report_config cos \
+                                            --report_config obl \
+                                            --scan_output ${env.WORKSPACE}/${env.EVIDENCE}
+                                    """
+                                    archiveArtifacts allowEmptyArchive: true, artifacts: "${env.WORKSPACE}/${env.EVIDENCE}"
+                                }
+                            }
+                        }
+                    }
                 }
         
             }
