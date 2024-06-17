3347678b8a677dff2b11a613aaa5ec7afe4a71fe
Lukas G <20104521+zLukas@users.noreply.github.com>
2024-06-17 12:18:28 +0200

    shellcheck updated (#71)

--- jenkins/scripts/shellcheck.sh
+++ jenkins/scripts/shellcheck.sh
@@ -1,10 +1,23 @@
-#!/bin/bash -x
+#! /bin/bash
 
-BASH_FILES=$(find . -name "*.sh")
+DIRS_TO_SCAN=( "./" 
+               "./debug_docker/"
+               "./doc"
+               "./patches"
+               "./pipelines"
+               "./tests"
+)
 OPTIONS="-s bash"
 OUT_FILE="shellcheck_output.log"
-
 rm -rf shellcheck_logs
 mkdir -p shellcheck_logs
 
-shellcheck ${OPTIONS} ${BASH_FILES} | tee  shellcheck_logs/${OUT_FILE}
+
+for _DIR in "${DIRS_TO_SCAN[@]}"
+do  
+    echo "*********** SCANNING ${_DIR} DIRECTORY... ***********" | tee -a shellcheck_logs/${OUT_FILE}
+    BASH_FILES=$(find ${_DIR} -maxdepth 1  -name "*.sh")
+    if [ -n "${BASH_FILES}" ]; then shellcheck ${OPTIONS} ${BASH_FILES} >>  shellcheck_logs/${OUT_FILE}; fi
+    
+done
+echo "*********** SCANNING DONE ***********" >> shellcheck_logs/${OUT_FILE}
