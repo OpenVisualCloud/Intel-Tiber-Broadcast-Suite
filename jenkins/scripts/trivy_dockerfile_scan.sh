daf07dc5bc878f977134bfc934364d384ae678e0
zLukas <lukasz.grab@intel.com>
2024-11-05 10:16:11 +0100

    trivy scan simplified

--- jenkins/scripts/trivy_dockerfile_scan.sh
+++ jenkins/scripts/trivy_dockerfile_scan.sh
@@ -7,29 +7,25 @@ chmod a+w Trivy
 chmod a+w Trivy/dockerfile
 touch Trivy/dockerfile/trivy_clean_reports_source_code
 touch Trivy/dockerfile/trivy_clean_reports_source_code_sbom
-TRIVY_PASS=true
 
-COMPONENTS_PASS=true
-trivy filesystem --severity HIGH,CRITICAL --ignore-unfixed --no-progress --exit-code 1 --scanners vuln --format json -o Trivy/dockerfile/Source_code_Trivy.json --timeout 30m ./Dockerfile  || COMPONENTS_PASS=false
-trivy convert --format template --template ${SCRIPT_DIR}/trivy_report_template.tmpl -o Trivy/dockerfile/Source_code_Trivy.txt Trivy/dockerfile/Source_code_Trivy.json
-if [ "$COMPONENTS_PASS" = "true" ]; then
-    echo Trivy/dockerfile/Trivy_idc.txt >> Trivy/dockerfile/trivy_clean_reports_source_code
-else
-    TRIVY_PASS=false
-fi
+trivy filesystem \
+      --severity HIGH,CRITICAL \
+      --ignore-unfixed \
+      --no-progress \
+      --exit-code 0 \
+      --scanners vuln \
+      --format table \
+      -o Trivy/dockerfile/Source_code_Trivy.txt --timeout 30m ./Dockerfile  || COMPONENTS_PASS=false
 
-SPDX_PASS=true
-trivy filesystem --no-progress --exit-code 1 --format spdx -o Trivy/dockerfile/Trivy_sbom.spdx ./Dockerfile || SPDX_PASS=false
-if [ "$SPDX_PASS" = "true" ]; then
-    echo Trivy/dockerfile/Trivy_sbom.spdx >> Trivy/dockerfile/trivy_clean_reports_source_code_sbom
-else
-    TRIVY_PASS=false
-fi
 
-echo $TRIVY_PASS>Trivy/dockerfile/trivy_source_code_pass
+trivy filesystem \
+      --no-progress \
+      --exit-code 0 \
+      --format spdx \
+      -o Trivy/dockerfile/Trivy_sbom.spdx ./Dockerfile || SPDX_PASS=false
 
-echo "creating summary ..."
-python3 jenkins/scripts/trivy_dockerfile_summary.py Trivy/dockerfile/Source_code_Trivy.json Trivy/dockerfile_scan_summary.csv
-column -t -s, Trivy/dockerfile_scan_summary.csv > Trivy/dockerfile_scan_summary.txt
+# echo "creating summary ..."
+# python3 jenkins/scripts/trivy_dockerfile_summary.py Trivy/dockerfile/Source_code_Trivy.json Trivy/dockerfile_scan_summary.csv
+# column -t -s, Trivy/dockerfile_scan_summary.csv > Trivy/dockerfile_scan_summary.txt
 
 echo "images scanning done"
