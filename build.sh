OpenVisualCloud/Intel-Tiber-Broadcast-Suite

commit b662ca28020c3b1cb51f9308c1760ddd68ada7db

Add: Add the first_run.sh local install capability (#132)
* Add: Add the first_run.sh local install capability

first_run.sh was only able to run with docker.
Add and describe the local running option to make
sure users can use Tiber without docker

first_run.sh
* Add option to run the first_run.sh with bare-metal
  installation using -l option.
* Adjust the first_run to be able to pull
  the nicctl directly from the repo as a fallback to
  the function pulling it from our containers

build.sh
* Adjust it to accommodate the MtlManager installation
  making sure that it will be available if
  mtl debian installation fails for the first_run
  local capability
* Fix a typo in build.sh add background to first.run manager

build.md
* Describe the first_run.sh local capability
* move the first_run description to the Run section
  to avoid confusion (first_run.sh needs to be
  executed after every reset)
* Fix abbreviations
* add vainfo check

pipelines
* Add lacking local run for some pipelines

build.sh
@@ -135,7 +135,7 @@ function download_install_debian {
     if sudo dpkg -i $folder_path/*.deb >>$log_file 2>&1; then
         return 0
     else
-        if [ -f "${folder_path}/download.tar.gz"]; then
+        if [ -f "${folder_path}/download.tar.gz" ]; then
             rm -f "${folder_path}/download.tar.gz"
         fi
         echo -e $CLEAR_LINE
@@ -373,6 +373,16 @@ function mtl_build {
         return 2
     fi
 
+    if ! (sudo apt-get -y install make m4 clang llvm zlib1g-dev libelf-dev libpcap-dev libcap-ng-dev gcc-multilib && \
+          cd ${LOCAL_INSTALL_DEPENDENCIES_DIRECTORY}/Media-Transport-Library/manager &&
+          meson setup build &&
+          meson compile -C build &&
+          sudo meson install -C build &&
+          cd -) >>$log_file 2>&1; then
+          echo
+        echo -e $RED[ERROR] MTL manager build failed $NC
+        return 2
+    fi
 }
 
 function dpdk_download_patch_build {
@@ -676,11 +686,11 @@ function install_locally {
         progress_function 50 100 dpdk_download_patch_build          || return 10
 
         if ! (download_install_debian $LINK_MTL_DEBIAN_v2204_ZIP mtl &&
-              progress_bar 55 &&
+              progress_bar 55 100 &&
               download_install_debian $LINK_JPEG_XS_DEBIAN_v2204_ZIP jpegxs &&
-              progress_bar 60 &&
+              progress_bar 60 100 &&
               download_install_debian $LINK_MCM_DEBIAN_v2204_ZIP mcm &&
-              progress_bar 65 &&
+              progress_bar 65 100 &&
               download_install_debian $LINK_FFMPEG_DEBIAN_v2204_ZIP ffmpeg); then
             progress_bar 50 100
             progress_function 55 100 mtl_build                      || return 9

first_run.sh
@@ -39,6 +39,7 @@ function copy_nicctl_script()
 {
     local script_result=""
     script_result="0"
+
     prompt 'Starting copy_nicctl_script sequence.'
     if [ ! -f "/usr/local/bin/nicctl.sh" ]; then
         docker create --name mtl-tmp mtl-manager:latest 2>&1 && \
@@ -46,6 +47,15 @@ function copy_nicctl_script()
         docker rm mtl-tmp 2>&1
         script_result="$?"
     fi
+
+    if [ "$script_result" != "0" ]; then
+        . versions.env 2>&1 &&
+        STRIPPED_VER=${MTL_VER#v} 2>&1 &&
+        wget -O /usr/local/bin/nicctl.sh https://raw.githubusercontent.com/OpenVisualCloud/Media-Transport-Library/refs/heads/maint-"${STRIPPED_VER}"/script/nicctl.sh  2>&1 &&
+        chmod +x /usr/local/bin/nicctl.sh 2>&1
+        script_result="$?"
+    fi
+
     if [ "$script_result" == "0" ]; then
         prompt 'Finished copy_nicctl_script sequence. Success.'
         return 0
@@ -140,7 +150,7 @@ function setup_mtl_manager_container
               --privileged --net=host \
               -v /var/run/imtl:/var/run/imtl \
               -v /sys/fs/bpf:/sys/fs/bpf \
-              mtl-manager:latest
+              mtl-manager:latest || return 2
         fi
     else
         docker run -d \
@@ -148,10 +158,31 @@ function setup_mtl_manager_container
           --privileged --net=host \
           -v /var/run/imtl:/var/run/imtl \
           -v /sys/fs/bpf:/sys/fs/bpf \
-          mtl-manager:latest
+          mtl-manager:latest || return 2
     fi
     prompt 'Finished run sequence for mtl-manager:latest image. Success.'
 }
+
+while getopts "l" opt; do
+    case ${opt} in
+    l )
+        setup_vfio_subsytem
+        setup_hugepages
+        setup_nic_virtual_functions
+
+        if ! setup_mtl_manager_container 1>/dev/null 2>&1 && ! pgrep -x "MtlManager" > /dev/null; then 
+            prompt 'Now starting Mtl Manager'
+            nohup sudo MtlManager > /dev/null 2>&1 &
+            prompt 'Mtl Manager running in background'
+        fi
+
+        exit 0
+    ;;
+    \? )
+        exit 1
+        ;;
+    esac
+done
 
 setup_jq_package
 setup_vfio_subsytem

pipelines/mcm_media_proxy_rx.sh
@@ -15,6 +15,19 @@ if [ -z "$VFIO_PORT_R" ]; then
     exit 1
 fi
 
+while getopts "l" opt; do
+    case ${opt} in
+        l )
+            echo "Media proxy cannot run without docker"
+            exit 0
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
+
  docker run -it \
     --privileged \
     -u 0:0 \

pipelines/mcm_media_proxy_tx.sh
@@ -15,6 +15,18 @@ if [ -z "$VFIO_PORT_T" ]; then
     exit 1
 fi
 
+while getopts "l" opt; do
+    case ${opt} in
+        l )
+            echo "Media proxy cannot run without docker"
+            exit 0
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
  docker run -it \
     --privileged \
     -u 0:0 \

pipelines/multiviewer_rx.sh
@@ -15,6 +15,35 @@ if [ -z "$VFIO_PORT_R" ]; then
     exit 1
 fi
 
+function help() {
+    echo "Usage: $0 [-l]"
+    echo
+    echo "Options:"
+    echo "  -l    Run the pipeline on bare metal locally."
+    echo
+    echo "For more information, please refer to docs/run.md."
+    exit 0
+}
+
+while getopts "lh" opt; do
+    case ${opt} in
+        l )
+            echo "Running pipeline on bare metal locally..."
+            ffmpeg -y \
+                -p_port "${VFIO_PORT_R}" -p_sip 192.168.2.3 -p_rx_ip 192.168.2.2 -udp_port 20000 -payload_type 112 -fps 25 -pix_fmt yuv422p10le -f mtl_st20p -i "0" \
+                /src/recv-multiviewer.yuv
+            exit 0
+            ;;
+        h )
+            help
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            help
+            ;;
+    esac
+done
+
 docker run -it \
    --user root\
    --privileged \

pipelines/multiviewer_tx.sh
@@ -15,6 +15,49 @@ if [ -z "$VFIO_PORT_T" ]; then
     exit 1
 fi
 
+function help() {
+    echo "Usage: $0 [-l]"
+    echo
+    echo "Options:"
+    echo "  -l    Run the pipeline on bare metal locally."
+    echo
+    echo "For more information, please refer to docs/run.md."
+    exit 0
+}
+
+while getopts "lh" opt; do
+    case ${opt} in
+        l )
+            echo "Running pipeline on bare metal locally..."
+            ffmpeg \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_1.yuv \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_2.yuv \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_1.yuv \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_2.yuv \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_1.yuv \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_2.yuv \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_1.yuv \
+              -video_size 1920x1080 -f rawvideo -pix_fmt yuv422p10le -i src/1080p_yuv422_10b_2.yuv \
+              -map 0:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20000 -payload_type 112 -fps 25 -f mtl_st20p - \
+              -map 1:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20001 -payload_type 112 -fps 25 -f mtl_st20p - \
+              -map 2:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20002 -payload_type 112 -fps 25 -f mtl_st20p - \
+              -map 3:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20003 -payload_type 112 -fps 25 -f mtl_st20p - \
+              -map 4:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20004 -payload_type 112 -fps 25 -f mtl_st20p - \
+              -map 5:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20005 -payload_type 112 -fps 25 -f mtl_st20p - \
+              -map 6:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20006 -payload_type 112 -fps 25 -f mtl_st20p - \
+              -map 7:v -p_port "${VFIO_PORT_T}" -p_sip 192.168.2.1 -p_tx_ip 192.168.2.2 -udp_port 20007 -payload_type 112 -fps 25 -f mtl_st20p -
+            exit 0
+            ;;
+        h )
+            help
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            help
+            ;;
+    esac
+done
+
 docker run -it \
    --user root\
    --privileged \

pipelines/upscale_process.sh
@@ -15,6 +15,37 @@ if [ -z "$VFIO_PORT_PROC" ]; then
     exit 1
 fi
 
+function help() {
+    echo "Usage: $0 [-l]"
+    echo
+    echo "Options:"
+    echo "  -l    Run the pipeline on bare metal locally."
+    echo
+    echo "For more information, please refer to docs/run.md."
+    exit 0
+}
+
+while getopts "lh" opt; do
+    case ${opt} in
+        l )
+            echo "Running pipeline on bare metal locally..."
+            ffmpeg -y \
+              -init_hw_device vaapi=va -init_hw_device opencl@va \
+              -p_port "${VFIO_PORT_PROC}" -p_sip 192.168.2.2 -p_rx_ip 192.168.2.1 -udp_port 20000 -payload_type 112 -fps 25 -pix_fmt yuv422p10le  -video_size 1920x1080 -f mtl_st20p -i "0" \
+              -vf "format=yuv420p,hwupload,raisr_opencl,hwdownload,format=yuv420p,format=yuv422p10le" \
+              -p_port "${VFIO_PORT_PROC}" -p_sip 192.168.2.2 -p_tx_ip 192.168.2.3 -udp_port 20000 -payload_type 112 -f mtl_st20p -
+            exit 0
+            ;;
+        h )
+            help
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            help
+            ;;
+    esac
+done
+
 docker run -it \
    --user root\
    --privileged \

pipelines/upscale_rx.sh
@@ -15,6 +15,36 @@ if [ -z "$VFIO_PORT_R" ]; then
     exit 1
 fi
 
+function help() {
+    echo "Usage: $0 [-l]"
+    echo
+    echo "Options:"
+    echo "  -l    Run the pipeline on bare metal locally."
+    echo
+    echo "For more information, please refer to docs/run.md."
+    exit 0
+}
+
+while getopts "lh" opt; do
+    case ${opt} in
+        l )
+            echo "Running pipeline on bare metal locally..."
+            ffmpeg -y \
+                -p_port "${VFIO_PORT_R}" -p_sip 192.168.2.3 -p_rx_ip 192.168.2.2 -udp_port 20000 -payload_type 112 -fps 25 -pix_fmt yuv422p10le  -video_size 3840x2160 -f mtl_st20p -i "0" \
+                src/recv-upscale.yuv
+            exit 0
+            ;;
+        h )
+            help
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            help
+            ;;
+    esac
+done
+
+
 docker run -it \
    --user root\
    --privileged \

commit 34dc1fc78fb90da680d95775d9d87ba52e14b805

Fix: Mtl dependency in build.sh (#136)
Build script while installing the mtl debian
in the ubuntu 22_04 -> debian route, has listed
pyton3-pip dependency wchich is not inlcuded in
the dependency installation script.

Add to the build.sh

Fixes: b662ca28020c3b1cb51f9308c1760ddd68ada7db

build.sh
@@ -220,6 +220,7 @@ function install_dependencies {
         libc6-dev \
         cuda-toolkit-12-6 \
         libnvidia-compute-550 \
+        python3-pip \
         libfdt-dev 1>>$log_file 2>&1; then
         echo
         echo -e $RED[ERROR] apt-get installing dependencies failed $NC