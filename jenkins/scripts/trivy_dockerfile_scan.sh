0bfe0ac3f4038b67cedb80dae86114827cb2bd36
zLukas <lukasz.grab@intel.com>
2024-11-12 10:50:37 +0100

    hotfix: trivy dockerfile scan updated according to docs

--- jenkins/scripts/trivy_dockerfile_scan.sh
+++ jenkins/scripts/trivy_dockerfile_scan.sh
@@ -1,33 +1,9 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 
 SCRIPT_DIR="jenkins/scripts"
 mkdir -p Trivy
 mkdir -p Trivy/dockerfile
 chmod a+w Trivy
 chmod a+w Trivy/dockerfile
-touch Trivy/dockerfile/trivy_clean_reports_source_code
-touch Trivy/dockerfile/trivy_clean_reports_source_code_sbom
 
-DOCKER_FILE=${1}
-
-trivy filesystem \
-      --severity HIGH,CRITICAL \
-      --ignore-unfixed \
-      --no-progress \
-      --exit-code 1 \
-      --scanners vuln \
-      --format table \
-      -o Trivy/dockerfile/Source_code_Trivy.txt --timeout 30m ./Dockerfile 
-
-
-trivy filesystem \
-      --no-progress \
-      --exit-code 2 \
-      --format spdx \
-      -o Trivy/dockerfile/Trivy_sbom.spdx ./Dockerfile 
-
-# echo "creating summary ..."
-# python3 jenkins/scripts/trivy_dockerfile_summary.py Trivy/dockerfile/Source_code_Trivy.json Trivy/dockerfile_scan_summary.csv
-# column -t -s, Trivy/dockerfile_scan_summary.csv > Trivy/dockerfile_scan_summary.txt
-
-echo "images scanning done"
+trivy conf --misconfig-scanners dockerfile -o Trivy/dockerfile/source_config.txt ./Dockerfile 
