bdb5a4bb5e532053678d30be34cd48f86b7f4322
zLukas <lukasz.grab@intel.com>
2024-11-13 14:28:17 +0100

    hadolint locally tested

--- jenkins/scripts/hadolint.sh
+++ jenkins/scripts/hadolint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -x
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 rm -rf Hadolint
 mkdir -p Hadolint
 
-hadolint -v -V --config ${SCRIPT_DIR}/hadolint_config.yaml > Hadolint/hadolint-Dockerfile
-hadolint --config ${SCRIPT_DIR}/hadolint_config.yaml --no-color Dockerfile 2>&1 > Hadolint/Dockerfile.log 
+hadolint -v -V --config jenkins/scripts/hadolint_config.yaml > Hadolint/hadolint-Dockerfile
+hadolint --config jenkins/scripts/hadolint_config.yaml --no-color Dockerfile 2>&1 > Hadolint/Dockerfile.log 
 exit 0
