558839f7b7fb6d16e76666982bce27fd44b1daab
Aly, Walid <walid.aly@intel.com>
2024-11-21 14:32:35 +0000

    client overview
    add client overview
    
    Signed-off-by: Aly, Walid <walid.aly@intel.com>

--- gRPC/compile.sh
+++ gRPC/compile.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+
 #SPDX-FileCopyrightText: Copyright (c) 2024 Intel Corporation
 #
 #SPDX-License-Identifier: BSD-3-Clause
-
-#!/bin/bash
 
 cmake -S . -B build && cd build && make && cd ..
 
