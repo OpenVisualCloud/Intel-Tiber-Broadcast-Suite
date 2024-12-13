24a8d4717e8bb041cad7c29dda64049e3be55183
Magda-Pytel <magdalena.pytel@intel.com>
2024-12-13 10:49:25 +0100

    Add nmos support (#27)
    * Add nmos support
    
    ---------
    
    Co-authored-by: Andrzej Wilczynski <andrzej.wilczynski@intel.com>

---
+++ nmos/docker/UserConfig.py
@@ -0,0 +1,25 @@
+#SPDX-FileCopyrightText: Copyright (c) 2024 Intel Corporation
+#
+#SPDX-License-Identifier: BSD-3-Clause
+
+#  Copyright 2024 Intel
+
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+
+#      http://www.apache.org/licenses/LICENSE-2.0
+
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# NOTICE: This file is inspired by the implementation in https://github.com/rhastie/easy-nmos/tree/master
+#         Licensed under Apache License, Version 2.0
+
+from . import Config as CONFIG
+
+CONFIG.ENABLE_HTTPS = False
+HEARTBEAT_INTERVAL = 5
