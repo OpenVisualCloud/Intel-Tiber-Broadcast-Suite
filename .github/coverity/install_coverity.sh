f734533a1ac34493e686726acb27031de624f39f
zLukas <lukasz.grab@intel.com>
2025-01-29 16:17:02 +0000

    fix syntax in coverity install script

--- .github/coverity/install_coverity.sh
+++ .github/coverity/install_coverity.sh
@@ -7,7 +7,7 @@ COVERITY_TOOL_FILE=coverity_tool.tgz
 
 if [ -e ${COVERITY_TOOL_FILE} ]; then
  echo coverity already installed, skipping installation
- exit(0)
+ exit 0
 fi 
 # Check if required environment variables are set
 if [ -z "$COVERITY_PROJECT" ] || [ -z "$COVERITY_TOKEN" ] || [ -z "$COVERITY_EMAIL" ] || [ -z "$LANGUAGE" ]; then
