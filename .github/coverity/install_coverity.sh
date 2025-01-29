e8bc171c05da3569a1ee7d423910ce62aa64554e
zLukas <lukasz.grab@intel.com>
2025-01-29 16:09:13 +0000

    improve insalation script, add docker commit command

--- .github/coverity/install_coverity.sh
+++ .github/coverity/install_coverity.sh
@@ -1,8 +1,14 @@
-#!/bin/bash -e
+#!/bin/bash -xe
+
 
 
 COVERITY_URL=https://scan.coverity.com/download/${LANGUAGE}/linux64
+COVERITY_TOOL_FILE=coverity_tool.tgz
 
+if [ -e ${COVERITY_TOOL_FILE} ]; then
+ echo coverity already installed, skipping installation
+ exit(0)
+fi 
 # Check if required environment variables are set
 if [ -z "$COVERITY_PROJECT" ] || [ -z "$COVERITY_TOKEN" ] || [ -z "$COVERITY_EMAIL" ] || [ -z "$LANGUAGE" ]; then
   echo "Error: COVERITY_PROJECT, COVERITY_TOKEN, COVERITY_EMAIL and LANGUAGE environment variables must be set."
@@ -12,7 +18,7 @@ fi
 # Download Coverity Scan
 echo "Downloading Coverity Scan..."
 curl -L ${COVERITY_URL} \
-  --output coverity_tool.tgz \
+  --output ${COVERITY_TOOL_FILE} \
   --data "token=${COVERITY_TOKEN}" \
   --data "project=${COVERITY_PROJECT}"
 
