3dcb18403f77d833a83375c66624cb4888b55308
zLukas <lukasz.grab@intel.com>
2025-01-30 08:56:15 +0000

    add coverity run script

---
+++ .github/coverity/run_coverity.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COV_VER="$(ls /opt/coverity/bin/)"
+COVERITY_BIN_DIR="/opt/coverity/bin/$COV_VER/bin"
+export JAVA_HOME="/opt/coverity/bin/$COV_VER/jre/bin/java"
+# Array to hold the PIDs of background jobs
+BUILD_SCRIPTS=($(ls .github/tools/coverity/*build_cmd.sh))
+
+PIDS=()
+mkdir -p cov
+rm -rf cov/*
+
+for SCRIPT in "${BUILD_SCRIPTS[@]}"; do 
+  # Run cov-build in the background and store the PID
+  cov-build --dir "cov/${NAME/_build_cmd.sh/}" "$SCRIPT" &
+  PIDS+=($!)
+done
+
+# Wait for all background jobs to complete
+for PID in "${PIDS[@]}"; do wait $PID; done
+
+echo "All cov-build processes have completed."
