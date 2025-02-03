6a70ae60a2e6b21bfef2f51bb072be8367663d3d
zLukas <lukasz.grab@intel.com>
2025-02-03 17:32:00 +0000

    update cov-build.sh

--- .github/coverity/cov-build.sh
+++ .github/coverity/cov-build.sh
@@ -7,8 +7,9 @@ ROOT_DIR="$(git rev-parse --show-toplevel)"
 
 
 function coverity_build(){
-  local NAME=${1/.sh/}
-  ${COV_BUILD} --dir "cov/${NAME}" "$1" | tee  ${NAME}.log
+  local NAME=${1}
+  local SCRIPT=${2}
+  ${COV_BUILD} --dir "cov/${NAME}" "${SCRIPT}" | tee  ${NAME}.log
 }
 
 
@@ -24,19 +25,19 @@ function build_nmos(){
 function build_nmos_cpp(){
   echo "building nmos-cpp"
   cd ${ROOT_DIR}/nmos
-  ${COV_BUILD} prepare-nmos-cpp.sh
+  ${COV_BUILD} nmos-cpp prepare-nmos-cpp.sh
 }
 
 function build_grpc(){
   echo "building gRPC"
   cd ${ROOT_DIR}/gRPC
-  ${COV_BUILD} compile.sh
+  ${COV_BUILD} grpc compile.sh
 }
 
 function build_launcher(){
   echo "building launcher"
   cd ${ROOT_DIR}/launcher
-  ${COV_BUILD} "go build -a -o manager cmd/main.go"
+  ${COV_BUILD} launcher "go build -a -o manager cmd/main.go"
 }
 
 if [ $# -ne 1 ]; then
