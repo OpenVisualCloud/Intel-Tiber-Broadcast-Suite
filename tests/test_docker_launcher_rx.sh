3382f2acc854ea1db943526cf12b7445e2c2b7c5
Wilczynski, Andrzej <andrzej.wilczynski@intel.com>
2025-02-04 12:56:05 +0000

    Updated build script, added scripts for tests
    Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>

---
+++ tests/test_docker_launcher_rx.sh
@@ -0,0 +1,20 @@
+docker run -it \
+   --user root\
+   --privileged \
+   --device=/dev/vfio:/dev/vfio \
+   --device=/dev/dri:/dev/dri \
+   --cap-add ALL \
+   -v "$(pwd)/recv":/videos/recv \
+   -v /usr/lib/x86_64-linux-gnu/dri:/usr/local/lib/x86_64-linux-gnu/dri/ \
+   -v /tmp/kahawai_lcore.lock:/tmp/kahawai_lcore.lock \
+   -v /dev/null:/dev/null \
+   -v /tmp/hugepages:/tmp/hugepages \
+   -v /hugepages:/hugepages \
+   -v /var/run/imtl:/var/run/imtl \
+   -e http_proxy="" \
+   -e https_proxy="" \
+   --network=my_net_801f0 \
+   --ip=192.168.2.5 \
+   --expose=20000-20170 \
+   --ipc=host -v /dev/shm:/dev/shm \
+      video_production_image 192.168.2.5 50052
