949cc7c74bf9b6fc09c1b4c9549ae287a9a0df52
Lukas G <20104521+zLukas@users.noreply.github.com>
2025-02-10 10:27:29 +0100

    minor formatting

--- .github/coverity/enviroment.sh
+++ .github/coverity/enviroment.sh
@@ -5,18 +5,18 @@ COV_CPP_VER="$(ls /opt/coverity/cxx)"
 COV_OTHER_VER="$(ls /opt/coverity/other)"
 COVERITY_CPP_BIN_DIR="/opt/coverity/cxx/$COV_CPP_VER/bin"
 COVERITY_OTHER_BIN_DIR="/opt/coverity/other/$COV_OTHER_VER/bin"
-COV_CPP_JAVA_HOM="${COVERITY_CPP_BIN_DIR}/jre/bin/java"
-COV_OTHER_JAVA_HOM="${COVERITY_OTHER_BIN_DIR}/jre/bin/java"
+COV_CPP_JAVA_HOME="${COVERITY_CPP_BIN_DIR}/jre/bin/java"
+COV_OTHER_JAVA_HOME="${COVERITY_OTHER_BIN_DIR}/jre/bin/java"
 
 function export_cpp_java(){ 
-  export JAVA_HOME="${COV_CPP_JAVA_HOM}"
+  export JAVA_HOME="${COV_CPP_JAVA_HOME}"
 }
 function export_other_java(){ 
-  export JAVA_HOME="${COV_OTHER_JAVA_HOM}"
+  export JAVA_HOME="${COV_OTHER_JAVA_HOME}"
 }
 function log_info(){ 
    echo "[INFO]: ${1}" 
 }
 function log_warning(){ 
-  echo "[WARNING]: $1 "
+  echo "[WARNING]: ${1} "
 }
