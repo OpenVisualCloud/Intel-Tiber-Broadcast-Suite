6df64c77512c50d3acee7f270c017f7b682b48ec
Andrzej Wilczynski <andrzej.wilczynski@intel.com>
2025-03-24 17:19:16 +0100

    Enable gpu hw processing, fixed infite wait for receivers in nmos (#90)
    * Enable gpu hw processing, fixed infite wait for receivers in nmos
    
    Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>
    
    * Fixed UT
    
    Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>
    
    ---------
    
    Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>

--- tests/test_docker_nmos_tx.sh
+++ tests/test_docker_nmos_tx.sh
@@ -74,6 +74,28 @@ case $1 in
       -v /dev/shm:/dev/shm \
       tiber-broadcast-suite-nmos-node config/intel-node-tx-2.json
     ;;
+  3)
+    docker run -it \
+      --user root \
+      --privileged \
+      --device=/dev/vfio:/dev/vfio \
+      --device=/dev/dri:/dev/dri \
+      --cap-add ALL \
+      -v "$(pwd)":/home/config/ \
+      -v /usr/lib/x86_64-linux-gnu/dri:/usr/local/lib/x86_64-linux-gnu/dri/ \
+      -v /tmp/kahawai_lcore.lock:/tmp/kahawai_lcore.lock \
+      -v /dev/null:/dev/null \
+      -v /tmp/hugepages:/tmp/hugepages \
+      -v /hugepages:/hugepages \
+      -v /var/run/imtl:/var/run/imtl \
+      -e http_proxy="" \
+      -e https_proxy="" \
+      -e VFIO_PORT_TX=0000:31:01.3 \
+      --network=host \
+      --ipc=host \
+      -v /dev/shm:/dev/shm \
+      tiber-broadcast-suite-nmos-node config/intel-node-tx-h265.json
+    ;;
   *)
     echo "Invalid number: $1. Please provide 0, 1 or 2."
     exit 1
