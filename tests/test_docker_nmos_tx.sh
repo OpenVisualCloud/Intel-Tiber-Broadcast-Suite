c4b0f3314af83b9b43b7168089a805a069195194
Your Name <you@example.com>
2025-03-28 13:59:53 +0100

    Added the ability to select config depending on pixel format. Added h264 configuration files

--- tests/test_docker_nmos_tx.sh
+++ tests/test_docker_nmos_tx.sh
@@ -7,6 +7,32 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+if [ -z "$2" ]; then
+  echo "Usage: $0 $1 <video_type>"
+  exit 1
+fi
+
+case $2 in
+  "raw")
+    format=""
+    ;;
+  "h264")
+    format="-h264"
+    ;;
+  "h265")
+    format="-h265"
+    ;;
+  *)
+    echo "Invalid option: $2"
+    echo "Valid options are: raw, h264, h265"
+    exit 1
+    ;;
+esac
+
+config_file="intel-node-tx${format}.json"
+config_file1="intel-node-tx-1${format}.json"
+config_file2="intel-node-tx-2${format}.json"
+
 case $1 in
   0)
     docker run -it \
@@ -28,7 +54,7 @@ case $1 in
       --network=host \
       --ipc=host \
       -v /dev/shm:/dev/shm \
-      tiber-broadcast-suite-nmos-node config/intel-node-tx.json
+      tiber-broadcast-suite-nmos-node config/$config_file
     ;;
   1)
     docker run -it \
@@ -50,7 +76,7 @@ case $1 in
       --network=host \
       --ipc=host \
       -v /dev/shm:/dev/shm \
-      tiber-broadcast-suite-nmos-node config/intel-node-tx-1.json
+      tiber-broadcast-suite-nmos-node config/$config_file1
     ;;
   2)
     docker run -it \
@@ -72,7 +98,7 @@ case $1 in
       --network=host \
       --ipc=host \
       -v /dev/shm:/dev/shm \
-      tiber-broadcast-suite-nmos-node config/intel-node-tx-2.json
+      tiber-broadcast-suite-nmos-node config/$config_file2
     ;;
   3)
     docker run -it \
