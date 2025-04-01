86b00b83d64da9a4ab1b283092663adb5466eada
zLukas <lukasz.grab@intel.com>
2025-04-01 15:08:05 +0000

    improve logging process

--- .github/coverity/fix.sh
+++ .github/coverity/fix.sh
@@ -1,14 +1,14 @@
-echo "#fix me " | cat - .github/coverity/cov_build > temp && mv temp .github/coverity/cov_build
+echo "#fix me " | cat - .github/coverity/cov_build.sh > temp && mv temp .github/coverity/cov_build.sh
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git remote set-url origin https://x-access-token:${TOKEN}@github.com/${REPO}
 git checkout -b cov-build-fix
-git add .github/coverity/cov_build
+git add .github/coverity/cov_build.sh
 git commit -m "Add fix comment to .github/coverity/cov_build"
 git push origin cov-build-fix
 if gh pr list --search "cov-build fix" --json title | grep -q '"title": "cov-build fix"'; then
   echo "PR with title 'cov-build fix' already exists. Skipping creation."
 else
-  gh pr create --title "cov-build fix" --body "Automatically created PR to address build failure" --base main
+  gh pr create --title "cov-build fix" --body "Automatically created PR to address coverity build failure" --base main
   gh pr comment --body "CC @zLukas"
 fi
