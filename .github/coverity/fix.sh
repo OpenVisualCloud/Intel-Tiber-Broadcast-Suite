c4779eee7176741399b867ea6a0442ffe3cd2d80
zLukas <lukasz.grab@intel.com>
2025-04-01 14:56:43 +0000

    update fix script to mension zLukas

--- .github/coverity/fix.sh
+++ .github/coverity/fix.sh
@@ -1,8 +1,13 @@
-echo "#fix me " | cat - .github/coverity_cov_build > temp && mv temp .github/coverity_cov_build
+echo "#fix me " | cat - .github/coverity/cov_build > temp && mv temp .github/coverity/cov_build
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git checkout -b cov-build-fix
-git add .github/coverity_cov_build
-git commit -m "Add fix comment to .github/coverity_cov_build"
+git add .github/coverity/cov_build
+git commit -m "Add fix comment to .github/coverity/cov_build"
 git push origin cov-build-fix
-gh pr create --title "cov-build fix" --body "Automatically created PR to address build failure" --base main
+if gh pr list --search "cov-build fix" --json title | grep -q '"title": "cov-build fix"'; then
+  echo "PR with title 'cov-build fix' already exists. Skipping creation."
+else
+  gh pr create --title "cov-build fix" --body "Automatically created PR to address build failure" --base main
+  gh pr comment --body "CC @zLukas"
+fi
