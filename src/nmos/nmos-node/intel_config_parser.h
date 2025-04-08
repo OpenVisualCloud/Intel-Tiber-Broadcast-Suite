bf1e6af00215e1b4892841d5779b7bdaa203d80f
zLukas <lukasz.grab@intel.com>
2025-04-08 09:43:42 +0000

    update src folder with main

--- src/nmos/nmos-node/intel_config_parser.h
+++ src/nmos/nmos-node/intel_config_parser.h
@@ -4,26 +4,26 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
- #ifndef INTEL_CONFIG_PARSER_H
- #define INTEL_CONFIG_PARSER_H
- 
- #include <cpprest/json.h>
- #include "config_params.hpp"
- 
- class ConfigManager {
- public:
-     ConfigManager() = default;
-     int parse_json_file(const std::string& file_path);
-     void print_config() const;
-     const Config& get_config() const;
-     std::pair<int, int> get_framerate(const Stream& stream) const;
- 
- protected:
-     Config config;
-     Video parse_video(const web::json::value& video_data) const;
-     Audio parse_audio(const web::json::value& audio_data) const;
-     StreamType parse_stream_type(const web::json::value& stream_type_data) const;
-     Stream parse_stream(const web::json::value& stream_data) const;
- };
- 
- #endif // INTEL_CONFIG_PARSER_H
+#ifndef INTEL_CONFIG_PARSER_H
+#define INTEL_CONFIG_PARSER_H
+
+#include <cpprest/json.h>
+#include "config_params.hpp"
+
+class ConfigManager {
+public:
+    ConfigManager() = default;
+    int parse_json_file(const std::string& file_path);
+    void print_config() const;
+    const Config& get_config() const;
+    std::pair<int, int> get_framerate(const Stream& stream) const;
+
+protected:
+    Config config;
+    Video parse_video(const web::json::value& video_data) const;
+    Audio parse_audio(const web::json::value& audio_data) const;
+    StreamType parse_stream_type(const web::json::value& stream_type_data) const;
+    Stream parse_stream(const web::json::value& stream_data) const;
+};
+
+#endif // INTEL_CONFIG_PARSER_H
