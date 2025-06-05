7cb418e6bb07680927724ad07efe24523dcc15a9
Magdalena Pytel <magdalena.pytel@intel.com>
2025-06-05 15:16:09 +0200

    Add enhancments for bcs launcher to be testable

--- launcher/resources_library/parser/parser.go
+++ launcher/resources_library/parser/parser.go
@@ -14,54 +14,6 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// type WorkloadConfig struct {
-// 	FfmpegPipeline FfmpegPipelineConfig `yaml:"ffmpegPipeline"`
-// 	NmosClient     NmosClientConfig     `yaml:"nmosClient"`
-// }
-
-// type Volumes struct {
-// 	Videos       string `yaml:"videos"`
-// 	Dri          string `yaml:"dri"`
-// 	Kahawai      string `yaml:"kahawai"`
-// 	Devnull      string `yaml:"devnull"`
-// 	TmpHugepages string `yaml:"tmpHugepages,omitempty"`
-// 	Hugepages    string `yaml:"hugepages,omitempty"`
-// 	Imtl         string `yaml:"imtl"`
-// 	Shm          string `yaml:"shm"`
-// }
-
-// type Devices struct {
-// 	Vfio string `yaml:"vfio"`
-// 	Dri  string `yaml:"dri"`
-// }
-
-// type FfmpegPipelineConfig struct {
-// 	Name                 string        `yaml:"name"`
-// 	ImageAndTag          string        `yaml:"imageAndTag"`
-// 	GRPCPort             int           `yaml:"gRPCPort"`
-// 	EnvironmentVariables []string      `yaml:"environmentVariables"`
-// 	Volumes              Volumes       `yaml:"volumes"`
-// 	Devices              Devices       `yaml:"devices"`
-// 	Network              NetworkConfig `yaml:"custom_network"`
-// }
-
-// type NmosClientConfig struct {
-// 	Name                    string        `yaml:"name"`
-// 	ImageAndTag             string        `yaml:"imageAndTag"`
-// 	EnvironmentVariables    []string      `yaml:"environmentVariables"`
-// 	NmosConfigPath          string        `yaml:"nmosConfigPath"`
-// 	NmosConfigFileName      string        `yaml:"nmosConfigFileName"`
-// 	Network                 NetworkConfig `yaml:"custom_network"`
-// 	NmosPort                int           `yaml:"nmosPort"`
-// 	FfmpegConnectionAddress string        `yaml:"ffmpegConnectionAddress"`
-// 	FfmpegConnectionPort    string        `yaml:"ffmpegConnectionPort"`
-// }
-
-//	type NetworkConfig struct {
-//		Enable bool   `yaml:"enable"`
-//		Name   string `yaml:"name"`
-//		IP     string `yaml:"ip"`
-//	}
 type Config struct {
 	ModeK8s       bool          `yaml:"k8s"`
 	Configuration Configuration `yaml:"configuration"`
